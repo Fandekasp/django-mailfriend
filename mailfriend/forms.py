d0b4893188696e433b5a438925f429f696a44ffb
Justin Quick <justquick@gmail.com>
2010-06-28 14:03:45 -0400

    engrish good

--- mailfriend/forms.py
+++ mailfriend/forms.py
@@ -31,7 +31,7 @@ class MailedItemForm(forms.ModelForm):
     def clean_mailed_to(self):
         to = split(self.cleaned_data['mailed_to'])
         if len(to) > MAX:
-            raise forms.ValidationError(_(u'You can only send to %s addresses at a time' % MAX))
+            raise forms.ValidationError(_(u'You can only send up to %s addresses at a time' % MAX))
         for address in to:
             if not email_re.match(address):
                 raise forms.ValidationError(_(u'Invalid e-mail address "%s"') % address)
