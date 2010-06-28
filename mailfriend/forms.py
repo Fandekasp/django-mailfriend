ca2471ff65724bde05052655660d96e71b2c8db3
Justin Quick <justquick@gmail.com>
2010-06-28 13:58:32 -0400

    added MAILFRIEND_MAX_ADDRESSES setting to limit the number of addresses you can mail to on any given request

--- mailfriend/forms.py
+++ mailfriend/forms.py
@@ -1,12 +1,12 @@
 from django import forms
+from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
-try:
-    from django.core.validators import email_re
-except:
-    from django.forms.fields import email_re
+from django.forms.fields import email_re
 
 from mailfriend.models import MailedItem
 from mailfriend.utils import generic_object_get, split
+
+MAX = getattr(settings, 'MAILFRIEND_MAX_ADDRESSES', 3)
 
 class MailedItemForm(forms.ModelForm):
     class Meta:
@@ -29,7 +29,10 @@ class MailedItemForm(forms.ModelForm):
         return generic_object_get(ct_pk, obj_pk)
         
     def clean_mailed_to(self):
-        for address in split(self.cleaned_data['mailed_to']):
+        to = split(self.cleaned_data['mailed_to'])
+        if len(to) > MAX:
+            raise forms.ValidationError(_(u'You can only send to %s addresses at a time' % MAX))
+        for address in to:
             if not email_re.match(address):
                 raise forms.ValidationError(_(u'Invalid e-mail address "%s"') % address)
         return self.cleaned_data['mailed_to']
